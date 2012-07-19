7377484371926cccd4890c5d81d5d3ddf6ae57af
Gaston Keller <gurtaj@atlantis>
2012-07-19 11:53:36 -0400

    Refactored VMPlacementPolicyFFDGreen.

--- extras/edu/uwo/csd/dcsim/extras/policies/VMPlacementPolicyFFDGreen.java
+++ extras/edu/uwo/csd/dcsim/extras/policies/VMPlacementPolicyFFDGreen.java
@@ -2,16 +2,11 @@ package edu.uwo.csd.dcsim.extras.policies;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 
-import edu.uwo.csd.dcsim.DCUtilizationMonitor;
-import edu.uwo.csd.dcsim.DataCentre;
-import edu.uwo.csd.dcsim.common.Utility;
+import edu.uwo.csd.dcsim.*;
 import edu.uwo.csd.dcsim.core.Simulation;
 import edu.uwo.csd.dcsim.host.Host;
 import edu.uwo.csd.dcsim.host.comparator.HostComparator;
-import edu.uwo.csd.dcsim.management.VMPlacementPolicy;
-import edu.uwo.csd.dcsim.vm.*;
 
 /**
  * Implements a First Fit Decreasing algorithm for a Green Strategy, where it 
@@ -22,81 +17,13 @@ import edu.uwo.csd.dcsim.vm.*;
  * @author Gaston Keller
  *
  */
-public class VMPlacementPolicyFFDGreen extends VMPlacementPolicy {
+public class VMPlacementPolicyFFDGreen extends VMPlacementPolicyGreedy {
 
-	protected DataCentre dc;
-	protected DCUtilizationMonitor utilizationMonitor;
-	protected double lowerThreshold;
-	protected double upperThreshold;
-	protected double targetUtilization;
-	
+	/**
+	 * Creates an instance of VMPlacementPolicyFFDGreen.
+	 */
 	public VMPlacementPolicyFFDGreen(Simulation simulation, DataCentre dc, DCUtilizationMonitor utilizationMonitor, double lowerThreshold, double upperThreshold, double targetUtilization) {
-		super(simulation);
-		this.dc = dc;
-		this.utilizationMonitor = utilizationMonitor;
-		this.lowerThreshold = lowerThreshold;
-		this.upperThreshold = upperThreshold;
-		this.targetUtilization = targetUtilization;
-	}
-	
-	/**
-	 * Places a new VM in the most efficient (first factor) and highly 
-	 * loaded (second factor) host with enough spare capacity to take the VM 
-	 * without exceeding the _targetUtilization_ threshold.
-	 */
-	@Override
-	public boolean submitVM(VMAllocationRequest vmAllocationRequest) {
-		
-		// Categorize hosts.
-		ArrayList<Host> partiallyUtilized = new ArrayList<Host>();
-		ArrayList<Host> underUtilized = new ArrayList<Host>();
-		ArrayList<Host> empty = new ArrayList<Host>();
-		
-		this.classifyHosts(partiallyUtilized, underUtilized, empty);
-		
-		// Create target hosts list.
-		ArrayList<Host> targets = this.orderTargetHosts(partiallyUtilized, underUtilized, empty);
-		
-		Host allocatedHost = null;
-		for (Host target : targets) {
-			 if (target.isCapable(vmAllocationRequest.getVMDescription()) &&															// target is capable
-			 	target.hasCapacity(vmAllocationRequest) &&																				// target has capacity
-			 	(target.getCpuManager().getCpuInUse() + vmAllocationRequest.getCpu()) / target.getTotalCpu() <= targetUtilization) {	// target will not be stressed
-				 
-				 allocatedHost = target;
-				 break;
-			 }
-		}
-		
-		return submitVM(vmAllocationRequest, allocatedHost);
-	}
-
-	/**
-	 * Classifies hosts as Partially-Utilized, Underutilized or Empty based on 
-	 * the hosts' average CPU utilization over the last CPU load monitoring 
-	 * window (see DCUtilizationMonitor).
-	 */
-	protected void classifyHosts(ArrayList<Host> partiallyUtilized, ArrayList<Host> underUtilized, ArrayList<Host> empty) {
-		ArrayList<Host> hostList = dc.getHosts();
-		
-		for (Host host : hostList) {
-			// Calculate host's avg CPU utilization in the last window of time.
-			LinkedList<Double> hostUtilValues = this.utilizationMonitor.getHostInUse(host);
-			double avgCpuInUse = 0;
-			for (Double x : hostUtilValues) {
-				avgCpuInUse += x;
-			}
-			avgCpuInUse = avgCpuInUse / this.utilizationMonitor.getWindowSize();
-			double avgCpuUtilization = Utility.roundDouble(avgCpuInUse / host.getCpuManager().getTotalCpu());
-			
-			if (host.getVMAllocations().size() == 0) {
-				empty.add(host);
-			} else if (avgCpuUtilization < lowerThreshold) {
-				underUtilized.add(host);
-			} else if (avgCpuUtilization <= upperThreshold) {
-				partiallyUtilized.add(host);
-			}
-		}
+		super(simulation, dc, utilizationMonitor, lowerThreshold, upperThreshold, targetUtilization);
 	}
 
 	/**
@@ -108,6 +35,7 @@ public class VMPlacementPolicyFFDGreen extends VMPlacementPolicy {
 	 * Returns Partially-utilized and Underutilized hosts first, followed by 
 	 * Empty hosts.
 	 */
+	@Override
 	protected ArrayList<Host> orderTargetHosts(ArrayList<Host> partiallyUtilized,	ArrayList<Host> underUtilized, ArrayList<Host> empty) {
 		ArrayList<Host> targets = new ArrayList<Host>();
 		
@@ -126,23 +54,6 @@ public class VMPlacementPolicyFFDGreen extends VMPlacementPolicy {
 		targets.addAll(empty);
 		
 		return targets;
-	}
-
-	/**
-	 * Places a set of new VMs, one by one, in the most efficient (first 
-	 * factor) and highly loaded (second factor) hosts with enough spare 
-	 * capacity to take the VMs without exceeding the _targetUtilization_ 
-	 * threshold.
-	 */
-	@Override
-	public boolean submitVMs(ArrayList<VMAllocationRequest> vmAllocationRequests) {
-		for (VMAllocationRequest request : vmAllocationRequests) {
-			if (!submitVM(request)) {
-				return false;
-			}
-		}
-		
-		return true;
 	}
 
 }
