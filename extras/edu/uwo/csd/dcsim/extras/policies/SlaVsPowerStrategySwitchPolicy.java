a799fb069d0c3e049bd56719a3716499bfb9dcd9
Michael Tighe <mtighe2@uwo.ca>
2012-08-07 23:45:42 -0400

    Modified strategy switching policy to use DCUtilizationMonitor values for SLA and power

--- extras/edu/uwo/csd/dcsim/extras/policies/SlaVsPowerStrategySwitchPolicy.java
+++ extras/edu/uwo/csd/dcsim/extras/policies/SlaVsPowerStrategySwitchPolicy.java
@@ -1,14 +1,9 @@
 package edu.uwo.csd.dcsim.extras.policies;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 import edu.uwo.csd.dcsim.*;
 import edu.uwo.csd.dcsim.common.ObjectBuilder;
 import edu.uwo.csd.dcsim.core.*;
 import edu.uwo.csd.dcsim.core.metrics.AggregateMetric;
-import edu.uwo.csd.dcsim.host.Host;
-import edu.uwo.csd.dcsim.host.comparator.HostComparator;
 import edu.uwo.csd.dcsim.management.VMPlacementPolicy;
 
 public class SlaVsPowerStrategySwitchPolicy implements Daemon {
@@ -135,65 +130,6 @@ public class SlaVsPowerStrategySwitchPolicy implements Daemon {
 		dc.setVMPlacementPolicy(powerPlacementPolicy);
 	}
 	
-	private double calculateOptimalCpuPerPower() {
-		
-		//create new list of all of the Hosts in the datacentre. We create a new list as we are going to resort it
-		ArrayList<Host> hosts = new ArrayList<Host>(dc.getHosts());
-		
-		//sort hosts by power efficiency, descending
-		Collections.sort(hosts, HostComparator.EFFICIENCY);
-		Collections.reverse(hosts);
-		
-		
-		/*
-		 * Calculate the theoretical optimal power consumption give the current workload.
-		 * 
-		 * To calculate this, we first consider the total of all CPU shares currently in use in the datacentre as a single
-		 * value that can be divided arbitrarily among hosts. We set 'cpuRemaining' to this value.
-		 * 
-		 * We then sort all Hosts in the data centre by power efficiency, starting with the most efficient host. We remove 
-		 * the number of CPU shares that the most efficient host possesses from cpuRemaining, and add the power that the host
-		 * would consume given 100% load to the optimal power consumption. We then move on to the next most efficient host
-		 * until cpuRemaining = 0 (all cpu has been assigned to a host).
-		 * 
-		 * The final host will probably not be entirely filled by the cpuRemaining still left to assign. If this is the case,
-		 * we calculate what the CPU utilization of the host would be given that all of cpuRemaining is placed on the host, and use
-		 * this value to calculate the host's power consumption. This power consumption is then added to the optimal power consumption. 
-		 * 
-		 */
-		double optimalPowerConsumption = 0; //the optimal total power consumption given the current load
-		double cpuRemaining = dcMon.getDCInUse().getFirst(); //the amount of CPU still to be allocated to a host
-		
-		int i = 0; //current position in host list
-		while (cpuRemaining > 0) {
-			
-			//if there is more CPU left than available in the host
-			if (cpuRemaining >= hosts.get(i).getTotalCpu()) {
-				
-				//remove the full capacity of the host from the remaining CPU
-				cpuRemaining -= hosts.get(i).getTotalCpu();
-				
-				//add the host power consumption at 100% to the optimalPowerConsumption
-				optimalPowerConsumption += hosts.get(i).getPowerModel().getPowerConsumption(1);
-			} 
-			//else if the host has enough capacity to satisfy all remaining CPU
-			else {
-				
-				//calculate the host utilization
-				double util = cpuRemaining / hosts.get(i).getTotalCpu();
-				cpuRemaining = 0;
-				
-				//add the power consumption of the host at the calculated utilization level
-				optimalPowerConsumption += hosts.get(i).getPowerModel().getPowerConsumption(util);
-			}
-			
-			++i; //move to next host
-		}
-		
-		//optimal cpu-per-power is (current CPU in use / optimal power consumption)
-		return dcMon.getDCInUse().getFirst() / optimalPowerConsumption;
-	}
-	
 	@Override
 	public void run(Simulation simulation) {
 			
@@ -202,23 +138,24 @@ public class SlaVsPowerStrategySwitchPolicy implements Daemon {
 		 */
 		
 		/* 
-		 * Calculate the SLA metric. This is done by getting the total amount of SLA Violated work since the last time strategy switching ran and dividing it by
-		 * the total amount of incoming work over the same period.
+		 * Calculate the SLA metric. Values from the DC monitor are averaged over the window size. 
 		 */
-		double sla = (dcMon.getTotalSlavWork() - lastSlavWork) / (dcMon.getTotalWork() - lastWork);
-		lastSlavWork = dcMon.getTotalSlavWork();
-		lastWork = dcMon.getTotalWork();
+		double sla = 0;
+		
+		//calculate average over window
+		for (Double val : dcMon.getDCsla())
+			sla += val;
+		sla = sla / dcMon.getDCsla().size();
 		
 		
-		/* 
-		 * Calculate the power metric. The power metric used is the ratio of the current cpu-shares-per-watt to the optimal cpu-shares-per-watt. 
-		 * The optimal value is currently a static value that should be set as the cpu-shares-per-watt of the most energy efficient host in the
-		 * data centre when fully utilized. Note that this is not a perfect metric in a heterogeneous data centre: as load increases, the optimal
-		 * cpu-shares-per-watt will go up as the load exceeds the capacity of the set of most energy efficient servers. Therefore, the optimal
-		 * value should actually change dynamically with total load.
+		/*
+		 * Calculate optimal power ratio metric. Values from the DC monitor are averaged over the window size.
 		 */
-		double optimalCpuPerPower = calculateOptimalCpuPerPower();
-		double power = optimalCpuPerPower / (dcMon.getDCInUse().getFirst() / dcMon.getDCPower().getFirst());
+		double power = 0;
+		
+		for (Double val : dcMon.getDCOptimalPowerRatio())
+			power += val;
+		power = power / dcMon.getDCOptimalPowerRatio().size();
 		
 		if (currentPolicy == slaPolicy) {
 			//We are current running an SLA friendly policy. The goal of the SLA policy is to keep SLA below the slaNormal threshold.
