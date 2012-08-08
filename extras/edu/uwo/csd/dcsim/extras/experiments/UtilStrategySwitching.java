ad14c67d4c4273d3b53fbdffe88e73f8548f62dc
Graham <dfoste2@csd.uwo.ca>
2012-08-08 13:38:52 -0400

    Created UtilStrategySwitching.java and UtilStrategySwitchPolicy.java to separate out util vs sla/power switching experiments

---
+++ extras/edu/uwo/csd/dcsim/extras/experiments/UtilStrategySwitching.java
@@ -0,0 +1,197 @@
+package edu.uwo.csd.dcsim.extras.experiments;
+
+import java.util.Collection;
+
+import org.apache.log4j.*;
+
+import edu.uwo.csd.dcsim.*;
+import edu.uwo.csd.dcsim.common.SimTime;
+import edu.uwo.csd.dcsim.core.*;
+import edu.uwo.csd.dcsim.extras.policies.*;
+import edu.uwo.csd.dcsim.management.VMPlacementPolicy;
+
+/**
+ * This simulation switching strategies between a power-friendly and SLA-friendly strategy.
+ * 
+ * @author Michael Tighe
+ *
+ */
+public class FullStrategySwitching extends DCSimulationTask {
+
+	private static Logger logger = Logger.getLogger(FullStrategySwitching.class);
+	
+	private double slaHigh, slaNormal, powerHigh, powerNormal;
+	private double toPowerThreshold, toSlaThreshold;
+	
+	public static void main(String args[]) {
+		
+		Simulation.initializeLogging();
+		
+		Collection<DCSimulationTask> completedTasks;
+		SimulationExecutor<DCSimulationTask> executor = new SimulationExecutor<DCSimulationTask>();
+		
+		executor.addTask(new FullStrategySwitching("strat-switching-1", 6198910678692541341l));
+//		executor.addTask(new FullStrategySwitching("strat-switching-2", 5646441053220106016l));
+//		executor.addTask(new FullStrategySwitching("strat-switching-3", -5705302823151233610l));
+//		executor.addTask(new FullStrategySwitching("strat-switching-4", 8289672009575825404l));
+//		executor.addTask(new FullStrategySwitching("strat-switching-5", -4637549055860880177l));
+		
+		completedTasks = executor.execute();
+		
+		for(DCSimulationTask task : completedTasks) {
+			logger.info(task.getName());
+			IM2012TestEnvironment.printMetrics(task.getResults());
+			
+			DCSimulationTraceWriter traceWriter = new DCSimulationTraceWriter(task);
+			traceWriter.writeTrace();
+		}
+
+	}
+
+	/**
+	 * Constructs a Strategy Switching task with default thresholds
+	 * @param name
+	 * @param randomSeed
+	 */
+	public FullStrategySwitching(String name, long randomSeed) {
+		super(name, SimTime.days(10));					// 10-day simulation
+		this.setMetricRecordStart(SimTime.days(2));	// start on 3rd day (i.e. after 2 days)
+		this.setRandomSeed(randomSeed);
+		this.slaHigh = 0.003;
+		this.slaNormal = 0.001;
+		this.powerHigh = 1.4;
+		this.powerNormal = 1.3;
+		this.toPowerThreshold = 0;
+		this.toSlaThreshold = 0;
+	}
+	
+	/**
+	 * Constructs a Strategy Switching task with supplied thresholds for switching on sla violations and power efficiency
+	 * @param name
+	 * @param randomSeed
+	 * @param slaHigh
+	 * @param slaNormal
+	 * @param powerHigh
+	 * @param powerNormal
+	 */
+	public FullStrategySwitching(String name, long randomSeed, double slaHigh, double slaNormal, double powerHigh, double powerNormal){
+		super(name, SimTime.days(10));					// 10-day simulation
+		this.setMetricRecordStart(SimTime.days(2));	// start on 3rd day (i.e. after 2 days)
+		this.setRandomSeed(randomSeed);
+		this.slaHigh = slaHigh;
+		this.slaNormal = slaNormal;
+		this.powerHigh = powerHigh;
+		this.powerNormal = powerNormal;
+		this.toPowerThreshold = 0;
+		this.toSlaThreshold = 0;
+	}
+	
+	/**
+	 * Constructs a Strategy Switching task with supplied thresholds for switching on utilization slope
+	 * @param name
+	 * @param randomSeed
+	 * @param toPowerThreshold threshold which would cause a switch to the power strategy
+	 * @param toSlaThreshold threshold which would case a switch to the sla strategy
+	 */
+	public FullStrategySwitching(String name, long randomSeed, double toPowerThreshold, double toSlaThreshold){
+		super(name, SimTime.days(10));					// 10-day simulation
+		this.setMetricRecordStart(SimTime.days(2));	// start on 3rd day (i.e. after 2 days)
+		this.setRandomSeed(randomSeed);
+		this.slaHigh = 0.003;
+		this.slaNormal = 0.001;
+		this.powerHigh = 1.4;
+		this.powerNormal = 1.3;
+		this.toPowerThreshold = toPowerThreshold;
+		this.toSlaThreshold = toSlaThreshold;
+	}
+	 
+
+	@Override
+	public void setup(DataCentreSimulation simulation) {
+
+		// Create data centre (with default VM Placement policy).
+		DataCentre dc = IM2012TestEnvironment.createDataCentre(simulation);
+		simulation.addDatacentre(dc);
+		
+		// Create CPU load utilization monitor.
+		DCUtilizationMonitor dcMon = new DCUtilizationMonitor(simulation, 120000, 5, dc);
+		simulation.addMonitor(dcMon);
+		
+
+		// Create and start ServiceProducer.
+//		IM2012TestEnvironment.configureStaticServices(simulation, dc);
+		IM2012TestEnvironment.configureDynamicServices(simulation, dc);
+		
+		
+		/*
+		 * Create Power-friendly Strategy
+		 */
+		
+		// Set utilization thresholds.
+		double powerLower = 0.6;
+		double powerUpper = 0.95;	// 0.90
+		double powerTarget = 0.90;	// 0.85
+		
+		// Create and set desired VM Placement policy for the data centre.
+		VMPlacementPolicy powerVMPlacementPolicy = new VMPlacementPolicyFFDGreen(simulation, dc, dcMon, powerLower, powerUpper, powerTarget);
+		
+		// Relocation policy
+		VMRelocationPolicyFFIDGreen powerRelocationPolicy = new VMRelocationPolicyFFIDGreen(dc, dcMon, powerLower, powerUpper, powerTarget);
+		DaemonScheduler powerRelocationPolicyDaemon = new FixedIntervalDaemonScheduler(simulation, 600000, powerRelocationPolicy);
+		
+		// Consolidation policy
+		VMConsolidationPolicyFFDDIGreen powerConsolidationPolicy = new VMConsolidationPolicyFFDDIGreen(dc, dcMon, powerLower, powerUpper, powerTarget);
+		DaemonScheduler powerConsolidationPolicyDaemon = new FixedIntervalDaemonScheduler(simulation, 3600000, powerConsolidationPolicy);
+
+		DaemonSchedulerGroup powerDaemonGroup = new DaemonSchedulerGroup(simulation);
+		powerDaemonGroup.addDaemon(powerRelocationPolicyDaemon, 600000);
+		powerDaemonGroup.addDaemon(powerConsolidationPolicyDaemon, 3601000);
+		
+		
+		
+		/*
+		 * Create SLA-friendly Strategy
+		 */
+		
+		// Set utilization thresholds.
+		double slaLower = 0.6;
+		double slaUpper = 0.85;
+		double slaTarget = 0.8;
+		
+		// Create and set desired VM Placement policy for the data centre.
+		VMPlacementPolicy slaVMPlacementPolicy = new VMPlacementPolicyFFMSla(simulation, dc, dcMon, slaLower, slaUpper, slaTarget);
+		
+		// Relocation policy
+		VMRelocationPolicyFFIMSla slaRelocationPolicy = new VMRelocationPolicyFFIMSla(dc, dcMon, slaLower, slaUpper, slaTarget);
+		DaemonScheduler slaRelocationPolicyDaemon = new FixedIntervalDaemonScheduler(simulation, 600000, slaRelocationPolicy);
+		
+		// Consolidation policy
+		VMConsolidationPolicyFFDMISla slaConsolidationPolicy = new VMConsolidationPolicyFFDMISla(dc, dcMon, slaLower, slaUpper, slaTarget);
+		DaemonScheduler slaConsolidationPolicyDaemon = new FixedIntervalDaemonScheduler(simulation, 14400000, slaConsolidationPolicy);
+
+		DaemonSchedulerGroup slaDaemonGroup = new DaemonSchedulerGroup(simulation);
+		slaDaemonGroup.addDaemon(slaRelocationPolicyDaemon, 600000);
+		slaDaemonGroup.addDaemon(slaConsolidationPolicyDaemon, 14401000);
+		
+		/*
+		 * Configure strategy switching
+		 */
+		
+		//currently configured so that only SLA value is used 
+		SlaVsPowerStrategySwitchPolicy switchingPolicy = new SlaVsPowerStrategySwitchPolicy.Builder(dc, dcMon)
+			.slaPolicy(slaDaemonGroup, slaVMPlacementPolicy)
+			.powerPolicy(powerDaemonGroup, powerVMPlacementPolicy)
+			.startingPolicy(powerDaemonGroup)
+			.slaHigh(slaHigh)
+			.slaNormal(slaNormal)
+			.powerHigh(powerHigh)
+			.powerNormal(powerNormal)
+			.toPowerThreshold(toPowerThreshold)
+			.toSlaThreshold(toSlaThreshold)
+			.build();
+		
+		DaemonScheduler policyDaemon = new FixedIntervalDaemonScheduler(simulation, SimTime.minutes(30), switchingPolicy);
+		policyDaemon.start(SimTime.minutes(30) - SimTime.seconds(1)); 
+	}
+
+}
