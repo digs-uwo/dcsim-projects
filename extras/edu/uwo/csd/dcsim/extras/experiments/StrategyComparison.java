969040b2c0c8716c1f13573b037db6cdee97265d
Michael Tighe <tighe.michael@gmail.com>
2012-08-09 21:10:42 -0400

    added strategy comparison class

---
+++ extras/edu/uwo/csd/dcsim/extras/experiments/StrategyComparison.java
@@ -0,0 +1,20 @@
+package edu.uwo.csd.dcsim.extras.experiments;
+
+public class StrategyComparison {
+
+	public static void main(String args[]) {
+		
+	}
+	
+	public void compare() {
+		
+		//iterate over a set of random seeds (workload patterns)
+		//for each random seed
+			//run SLA-friendly and Power-friendly
+			//store best and worst SLA and PowerE values
+			
+			//run strat-under-test
+			//score results
+	}
+	
+}
