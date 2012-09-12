0f6ddaf206acb646c4880f8730bab177e6b22bfd
Michael Tighe <mtighe2@uwo.ca>
2012-09-12 12:08:06 -0400

    Committing final StrategyEvaluator state

--- extras/edu/uwo/csd/dcsim/extras/experiments/StrategyEvaluator.java
+++ extras/edu/uwo/csd/dcsim/extras/experiments/StrategyEvaluator.java
@@ -21,53 +21,74 @@ public class StrategyEvaluator {
 		
 		stratEval.generateRandomSeeds(100, 6198910678692541341l);	
 		
-		DescriptiveStatistics hybridStats = stratEval.evaluateInChunks(4,
+		DescriptiveStatistics slaStats = stratEval.evaluateInChunks(4,
 				new ObjectFactory<DCSimulationTask>() {
 
 					@Override
 					public DCSimulationTask newInstance() {
-						return new BalancedStrategy("hybrid");
+						return new SLAFriendlyStrategy("sla", 0);
 					}
 					
 				});
-	
-		DescriptiveStatistics spStats = stratEval.evaluateInChunks(4,
+		
+		DescriptiveStatistics powerStats = stratEval.evaluateInChunks(4,
 				new ObjectFactory<DCSimulationTask>() {
 
 					@Override
 					public DCSimulationTask newInstance() {
-						return new SlaPowerStrategySwitching("sp-dss", 0.008, 0.004, 1.3, 1.15);
+						return new GreenStrategy("power", 0);
 					}
 					
 				});
 		
-		DescriptiveStatistics utilStats = stratEval.evaluateInChunks(4,
-				new ObjectFactory<DCSimulationTask>() {
-
-					@Override
-					public DCSimulationTask newInstance() {
-						//UtilStrategySwitching thresholds: toPower=0.00255, toSla=0.00255
-						return new UtilStrategySwitching("util-dss", 0, 0.00255, 0.00255);
-					}
-					
-				});
+//		DescriptiveStatistics hybridStats = stratEval.evaluateInChunks(4,
+//				new ObjectFactory<DCSimulationTask>() {
+//
+//					@Override
+//					public DCSimulationTask newInstance() {
+//						return new BalancedStrategy("hybrid");
+//					}
+//					
+//				});
+//	
+//		DescriptiveStatistics spStats = stratEval.evaluateInChunks(4,
+//				new ObjectFactory<DCSimulationTask>() {
+//
+//					@Override
+//					public DCSimulationTask newInstance() {
+//						return new SlaPowerStrategySwitching("sp-dss", 0.008, 0.004, 1.3, 1.15);
+//					}
+//					
+//				});
+//		
+//		DescriptiveStatistics utilStats = stratEval.evaluateInChunks(4,
+//				new ObjectFactory<DCSimulationTask>() {
+//
+//					@Override
+//					public DCSimulationTask newInstance() {
+//						//UtilStrategySwitching thresholds: toPower=0.00255, toSla=0.00255
+//						return new UtilStrategySwitching("util-dss", 0, 0.00255, 0.00255);
+//					}
+//					
+//				});
+//		
+//		DescriptiveStatistics goalStats = stratEval.evaluateInChunks(4,
+//				new ObjectFactory<DCSimulationTask>() {
+//
+//					@Override
+//					public DCSimulationTask newInstance() {
+//						//worstSla= 0.01, worstEfficiency= 0.83
+//						return new DistanceToGoalStrategySwitching("goal-dss", 0, 0.01, 0.83);
+//					}
+//					
+//				});
 		
-		DescriptiveStatistics goalStats = stratEval.evaluateInChunks(4,
-				new ObjectFactory<DCSimulationTask>() {
-
-					@Override
-					public DCSimulationTask newInstance() {
-						//worstSla= 0.01, worstEfficiency= 0.83
-						return new DistanceToGoalStrategySwitching("goal-dss", 0, 0.01, 0.83);
-					}
-					
-				});
-		
-		
-		System.out.println("Hybrid: " + hybridStats.getMean() + "\n");
-		System.out.println("SP-DSS: " + spStats.getMean() + "\n");
-		System.out.println("Util-DSS: " + utilStats.getMean() + "\n");
-		System.out.println("Goal-DSS: " + goalStats.getMean() + "\n");
+		System.out.println("SLA: " + slaStats.getMean() + "\n");
+		System.out.println("Power: " + powerStats.getMean() + "\n");
+//		System.out.println("Hybrid: " + hybridStats.getMean() + "\n");
+//		System.out.println("SP-DSS: " + spStats.getMean() + "\n");
+//		System.out.println("Util-DSS: " + utilStats.getMean() + "\n");
+//		System.out.println("Goal-DSS: " + goalStats.getMean() + "\n");
 	
 		
 	}
