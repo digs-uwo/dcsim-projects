105e9058f3e931d727294cea39a31d7d51f56758
Michael Tighe <tighe.michael@gmail.com>
2013-02-25 12:37:55 -0500

    Created projects source folder to contain all future digs projects

---
+++ projects/edu/uwo/csd/dcsim/projects/im2013/HybridStrategyExperiment.java
@@ -0,0 +1,19 @@
+package edu.uwo.csd.dcsim.projects.im2013;
+
+import edu.uwo.csd.dcsim.SimulationTask;
+import edu.uwo.csd.dcsim.core.Simulation;
+
+public class HybridStrategyExperiment extends SimulationTask {
+
+	public HybridStrategyExperiment(String name, long duration) {
+		super(name, duration);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setup(Simulation simulation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
