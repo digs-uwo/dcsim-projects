95073dbbe25f7caa78ddd8b0595a8be7dba128c4
Gaston Keller <gurtaj@atlantis>
2013-03-11 18:47:59 -0400

    Created new project, *hierarchical*, to start developing managers and policies for Hierarchical Mgmt.

---
+++ projects/edu/uwo/csd/dcsim/projects/hierarchical/HierarchicalTestEnvironment.java
@@ -0,0 +1,9 @@
+package edu.uwo.csd.dcsim.projects.hierarchical;
+
+public class HierarchicalTestEnvironment {
+
+	public HierarchicalTestEnvironment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
