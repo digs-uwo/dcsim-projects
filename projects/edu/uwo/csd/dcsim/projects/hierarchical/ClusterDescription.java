86dfdda1a311e8a0f3aad48a313a0d0f2c9cfa03
Gaston Keller <gurtaj@atlantis>
2013-08-01 13:55:16 -0400

    Added method to obtain a (static) Power Efficiency metric from RackDescription and ClusterDescription.

--- projects/edu/uwo/csd/dcsim/projects/hierarchical/ClusterDescription.java
+++ projects/edu/uwo/csd/dcsim/projects/hierarchical/ClusterDescription.java
@@ -4,14 +4,18 @@ import edu.uwo.csd.dcsim.host.Cluster;
 
 public class ClusterDescription {
 
-	private int rackCount;
-	private RackDescription rackDescription;			// Number of hosts per rack, plus static resource capacity of a single Host.
+	private int rackCount;								// Number of Racks in the Cluster.
+	private RackDescription rackDescription;			// Physical description of a Rack.
+	private double powerEfficiency = 0;				// Total CPU units over power consumption when the Cluster is fully utilized.
 	
-	// power efficiency ???
+	// TODO Power efficiency only takes into account power consumption of the Hosts; it does not include the power consumption 
+	// of the Switches. For that purpose, we'd need HostDescription to provide the power consumption of the Host at full load, 
+	// and RackDescription and ClusterDescription to include number of Switches and their power consumption (or description).
 	
 	public ClusterDescription(Cluster cluster) {
 		rackCount = cluster.getRackCount();
 		rackDescription = new RackDescription(cluster.getRacks().get(0));
+		powerEfficiency = rackDescription.getPowerEfficiency() * rackCount;
 	}
 	
 	public int getRackCount() {
@@ -20,6 +24,10 @@ public class ClusterDescription {
 	
 	public RackDescription getRackDescription() {
 		return rackDescription;
+	}
+	
+	public double getPowerEfficiency() {
+		return powerEfficiency;
 	}
 
 }
--- projects/edu/uwo/csd/dcsim/projects/hierarchical/RackDescription.java
+++ projects/edu/uwo/csd/dcsim/projects/hierarchical/RackDescription.java
@@ -5,14 +5,18 @@ import edu.uwo.csd.dcsim.management.HostDescription;
 
 public class RackDescription {
 
-	private int hostCount;
+	private int hostCount;								// Number of Hosts in the Rack.
 	private HostDescription hostDescription;			// Static resource capacity of a single Host.
+	private double powerEfficiency = 0;				// Total CPU units over power consumption when the Rack is fully utilized.
 	
-	// power efficiency ???
+	// TODO Power efficiency only takes into account power consumption of the Hosts; it does not include the power consumption 
+	// of the Switches. For that purpose, we'd need HostDescription to provide the power consumption of the Host at full load, 
+	// and we'd need RackDescription to include number of Switches in the Rack and their power consumption (or description).
 	
 	public RackDescription(Rack rack) {
 		hostCount = rack.getHostCount();
 		hostDescription = new HostDescription(rack.getHosts().get(0));
+		powerEfficiency = hostDescription.getPowerEfficiency() * hostCount;
 	}
 	
 	public int getHostCount() {
@@ -21,6 +25,10 @@ public class RackDescription {
 	
 	public HostDescription getHostDescription() {
 		return hostDescription;
+	}
+	
+	public double getPowerEfficiency() {
+		return powerEfficiency;
 	}
 
 }
