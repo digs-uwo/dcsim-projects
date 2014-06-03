fa774be93f78d20216a21d49ad0b8831a35b209f
Gaston Keller <gurtaj@atlantis>
2014-06-03 12:03:26 -0400

    CNSM2014: Created new constructor for MigAcceptEvent and deprecated the old one.

--- src/edu/uwo/csd/dcsim/projects/hierarchical/events/MigAcceptEvent.java
+++ src/edu/uwo/csd/dcsim/projects/hierarchical/events/MigAcceptEvent.java
@@ -1,19 +1,30 @@
 package edu.uwo.csd.dcsim.projects.hierarchical.events;
 
 import edu.uwo.csd.dcsim.host.Host;
-import edu.uwo.csd.dcsim.management.*;
+import edu.uwo.csd.dcsim.management.AutonomicManager;
+import edu.uwo.csd.dcsim.management.VmStatus;
 import edu.uwo.csd.dcsim.management.events.MessageEvent;
 
 public class MigAcceptEvent extends MessageEvent {
 
 	private VmStatus vm;
 	private Host targetHost;
+	private AutonomicManager origin;			// Manager accepting the migration.
 	
+	@Deprecated
 	public MigAcceptEvent(AutonomicManager target, VmStatus vm, Host targetHost) {
 		super(target);
 		
 		this.vm = vm;
 		this.targetHost = targetHost;
+	}
+	
+	public MigAcceptEvent(AutonomicManager target, VmStatus vm, Host targetHost, AutonomicManager origin) {
+		super(target);
+		
+		this.vm = vm;
+		this.targetHost = targetHost;
+		this.origin = origin;
 	}
 	
 	public VmStatus getVm() {
@@ -22,6 +33,10 @@ public class MigAcceptEvent extends MessageEvent {
 	
 	public Host getTargetHost() {
 		return targetHost;
+	}
+	
+	public AutonomicManager getOrigin() {
+		return origin;
 	}
 
 }
